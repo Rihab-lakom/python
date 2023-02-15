0e2ffcb86cd2891864391ddced114e124325699b
rihabm <lakomrihab@gmail.com>
2023-02-15 17:00:29 +0100

    hello_world

--- fundamentals/hello_world.py
+++ fundamentals/hello_world.py
@@ -1,8 +1,23 @@
-print("Hello World!")
-x = "Hello Python"
-print(x)
-y = 42
-print(y)
-print("Hello World2")
+# 1. TASK: print "Hello World"
+print ( "hello world" )
+# 2. print "Hello Noelle!" with the name in a variable
+name = "rihab"
+print( "hello", name )	# with a comma
+name = "rihab"
+print( " hello " + name )	# with a +
+# 3. print "Hello 42!" with the number in a variable
+name = 42
+print( "hello", 42 )	# with a comma
+# print( " hello " + 42 )	# with a +	-- this one should give us an error!
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print(" i love to eat " + fave_food1 + " and " + fave_food2) # with .format()
+print(f'ilove to eat {fave_food1} and {fave_food2}') # with an f string
+
+
+
+
+
 
 
