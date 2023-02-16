fa341a2b9572aa0a61c17091857f3fcd4f0a1341
rihabm <lakomrihab@gmail.com>
2023-02-16 12:33:21 +0100

    function_basic1.py

---
+++ Python_Fundamental/fundamentals/function_basic1.py
@@ -0,0 +1,90 @@
+# # Create a function that accepts a number as an input. Return a new list that counts down by one,
+# #  from the number (as the 0th element) down to 0 (as the last element).
+
+
+# # def number():
+# #     return 5
+# #     return 10
+# # print(number())
+# # 
+# # 
+# # d
+# # def add(b,c):
+# #     print(b+c)
+# # print(add(1,2) + add(2,3))
+# def concatenate(b,c):
+#     return str(b)+str(c)
+# print(concatenate(2,5))
+# #pass
+# def number_of_oceans_or_fingers_or_continents():
+#     b = 100
+#     print(b)
+#     if b < 10:
+#         return 5
+#     else:
+#         return 10
+#     return 7
+# print(number_of_oceans_or_fingers_or_continents())
+# #pass
+# def number(b,c):
+#     if b < c:
+#         return 7
+#     else:
+#         return 14
+#     return 3
+# print(number(2,3))
+# print(number(5,3))
+# print(number(2,3) + number(5,3))
+# #pass
+# def addition(b,c):
+#     return b+c
+#     return 10
+# print(addition(3,5))
+# #pass
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+# print(b)
+# foobar()
+# print(b)
+# #pass
+
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# foobar()
+# print(b)
+#pass
+# #b13
+#  = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# b=foobar()
+# print(b)
+#pass
+#15
+def foo():
+    print(1)
+    x = bar()
+    print(x)
+    return 10
+def bar():
+    print(3)
+    return 5
+y = foo()
+print(y)
+
+
+
+
+
