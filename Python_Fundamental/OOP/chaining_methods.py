cbb99b7b5c5acf075347b600f5d58eff375ffce0
rihabm <lakomrihab@gmail.com>
2023-02-16 15:28:24 +0100

    chaining_methods

--- Python_Fundamental/OOP/chaining_methods.py
+++ Python_Fundamental/OOP/chaining_methods.py
@@ -7,6 +7,7 @@ class User():
         # the status is set to True by default
         self.is_rewards_member = False
         self.gold_card_points =0
+        
 # Methods:
     # display_info(self) - Have this method print all 
     # of the users' details on separate lines.
@@ -18,24 +19,30 @@ class User():
         print(f"age: {self.age}")
         print(f"Member: {self.is_rewards_member}")
         print(f"Current Points: {self.gold_card_points}")
+        return self
 
 #enroll(self) - Have this method change the user's member status to True and set their gold card points to 200.
     def enroll(self):
         self.is_rewards_member = True
         self.gold_card_points = 200
+        return self
 # spend_points(self, amount) - have this method decrease
 #  the user's points by the amount specified.
     def spend_points(self ,amount):
-         self.gold_card_points -= amount
-
+        self.gold_card_points -= amount
+        return self
 
 sam = User("Sam", "lee", "samlee@codingdojo.com", 30)
-sam.display_info()
-sam.enroll()
+# sam.display_info()
+# sam.enroll()
 riri = User("riri", "lkm", "ririlkm@gmail.com", 25)
 lili = User("lili", "lkm", "lililkm@gmail.com", 27)
-riri.spend_points(100)
-lili.spend_points(50)
-riri.display_info()
-lili.display_info()
-sam.display_info()
+# riri.spend_points(100)
+# lili.spend_points(50)
+# riri.display_info()
+# lili.display_info()
+# sam.display_info()
+#chaining methods#########
+sam.enroll().spend_points(50).display_info()
+riri.enroll().spend_points(80).display_info()
+
